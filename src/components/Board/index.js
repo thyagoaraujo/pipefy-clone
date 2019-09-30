723a3e2c02fc3d4e7b4fb596100fdb3e0fa75cf3
Thyago Araujo <thyagopcb.araujo@gmail.com>
2019-09-30 19:31:15 -0300

    add structure board, card, list

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import List from '../List';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Board() {
-  return <div />;
+  return (
+    <Container>
+      <List />
+      <List />
+      <List />
+      <List />
+    </Container>
+  );
 }
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container, Label } from './styles';
 
 export default function Card() {
-  return <div />;
+  return (
+    <Container>
+      <header>
+        <Label color="#7159c1" />
+      </header>
+      <p>Fazer a migração completa de servidor</p>
+      <img src="https://avatars0.githubusercontent.com/u/42300657?v=4" alt="" />
+    </Container>
+  );
 }
